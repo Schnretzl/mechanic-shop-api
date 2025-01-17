6e6dce0a0e9f62bbdc8e75b4456dda9903e98ed1
Schnretzl <wrhendley@gmail.com>
2025-01-17 13:55:55 -0600

    Add EditServiceTicketSchema

--- app/blueprints/service_tickets/schemas.py
+++ app/blueprints/service_tickets/schemas.py
@@ -5,11 +5,17 @@ from marshmallow import fields
 class Service_TicketSchema(ma.SQLAlchemyAutoSchema):
     mechanics = fields.Nested("MechanicSchema", many=True)
     customer = fields.Nested("CustomerSchema")
-    
     class Meta:
         model = Service_Ticket
         fields = ('id', 'VIN', 'service_date', 'service_desc', 'customer_id', 'mechanic_ids', 'customer', 'mechanics')
         
+class EditServiceTicketSchema(ma.Schema):
+    add_mechanic_ids = fields.List(fields.Int(), required=True)
+    remove_mechanic_ids = fields.List(fields.Int(), required=True)
+    class Meta:
+        fields = ('add_mechanic_ids', 'remove_mechanic_ids')
+    
 service_ticket_schema = Service_TicketSchema()
 service_tickets_schema = Service_TicketSchema(many=True)
-return_service_tickets_schema = Service_TicketSchema(exclude=['customer_id'], many=True)
+customer_service_tickets_schema = Service_TicketSchema(exclude=['customer_id', 'customer'], many=True)
+edit_service_ticket_schema = EditServiceTicketSchema()

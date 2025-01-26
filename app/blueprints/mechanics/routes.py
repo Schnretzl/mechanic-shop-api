2584f0ce348fe06eb1f39a5eb51ab44ffddd12cf
Schnretzl <wrhendley@gmail.com>
2025-01-26 16:25:08 -0600

    Change update_mechanic to not update for empty values

--- app/blueprints/mechanics/routes.py
+++ app/blueprints/mechanics/routes.py
@@ -69,7 +69,8 @@ def update_mechanic(mechanic_id):
         return jsonify(e.messages), 400
     
     for field, value in mechanic_data.items():
-        setattr(mechanic, field, value)
+        if value:
+            setattr(mechanic, field, value)
         
     db.session.commit()
     

9ba599eedb2a487cd1bbacb56a384e872cc6ed22
Schnretzl <wrhendley@gmail.com>
2025-01-27 18:16:22 -0600

    Add test for get_customer_tickets

--- tests/test_customer.py
+++ tests/test_customer.py
@@ -16,7 +16,7 @@ class TestCustomer(unittest.TestCase):
             db.session.commit()
         self.token = encode_token('admin')
         self.client = self.app.test_client()
-            
+
     def test_create_customer(self):
         customer_payload = {
             'name': 'John Doe',
@@ -28,7 +28,7 @@ class TestCustomer(unittest.TestCase):
         response = self.client.post('/customers/', json=customer_payload)
         self.assertEqual(response.status_code, 201)
         self.assertEqual(response.json['name'], 'John Doe')
-        
+
     def test_invalid_create_customer(self):
         customer_payload = {
             'name': 'John Doe',
@@ -39,7 +39,7 @@ class TestCustomer(unittest.TestCase):
         response = self.client.post('/customers/', json=customer_payload)
         self.assertEqual(response.status_code, 400)
         self.assertEqual(response.json['phone'], ['Missing data for required field.'])
-        
+
     def test_login_customer(self):
         credentials = {
             'email': 'test@email.com',
@@ -50,7 +50,7 @@ class TestCustomer(unittest.TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json['status'], 'success')
         return response.json['token']
-        
+
     def test_invalid_login_customer(self):
         credentials = {
             'email': 'bad_email@email.com',
@@ -60,7 +60,7 @@ class TestCustomer(unittest.TestCase):
         response = self.client.post('/customers/login', json=credentials)
         self.assertEqual(response.status_code, 401)
         self.assertEqual(response.json['message'], 'Invalid email or password.')
-        
+
     def test_update_customer(self):
         update_payload = {
             'name': 'Peter',
@@ -68,18 +68,23 @@ class TestCustomer(unittest.TestCase):
             'phone': '',
             'password': ''
         }
-        
+
         headers = {'Authorization': 'Bearer ' + self.test_login_customer()}
-        
+
         response = self.client.put('/customers/', json=update_payload, headers=headers)
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json['name'], 'Peter')
         self.assertEqual(response.json['email'], 'test@email.com')
-        
+
     def test_get_customers(self):
         response = self.client.get('/customers/')
         self.assertEqual(response.status_code, 200)
         self.assertEqual(response.json[0]['name'], 'test_user')
+
+    def test_get_customer_tickets(self):
+        headers = {'Authorization': 'Bearer ' + self.test_login_customer()}
+        response = self.client.get('/customers/my-tickets', headers=headers)
+        self.assertEqual(response.status_code, 200)
 
     def test_delete_customer(self):
         headers = {'Authorization': 'Bearer ' + self.test_login_customer()}

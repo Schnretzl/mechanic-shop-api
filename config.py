b173b37ed479494e7028fde817cbae2ada746577
Schnretzl <wrhendley@gmail.com>
2025-01-29 17:49:11 -0600

    Update for production

--- config.py
+++ config.py
@@ -1,10 +1,7 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 class DevelopmentConfig:
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD')
+    MYSQL_PASSWORD = os.environ.get('MYSQL_PASSWORD')
     SQLALCHEMY_DATABASE_URI = f'mysql+mysqlconnector://root:{MYSQL_PASSWORD}@localhost/mechanic_shop'
     DEBUG = True
     CACHE_TYPE = 'SimpleCache'
@@ -15,4 +12,5 @@ class TestingConfig:
     CACHE_TYPE = 'SimpleCache'
 
 class ProductionConfig:
-    pass
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI')
+    CACHE_TYPE = 'SimpleCache'
